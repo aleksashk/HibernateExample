8299069e5978b908a4f28e06f1cd9b1595a6a651
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-03-27 10:46:33 +0300

    add lombok

--- src/main/java/com/philimonov/hibernate/entity/Student.java
+++ src/main/java/com/philimonov/hibernate/entity/Student.java
@@ -1,5 +1,8 @@
 package com.philimonov.hibernate.entity;
 
+import lombok.Data;
+import lombok.ToString;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +12,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name="students")
+@Data
 public class Student {
 
     @Id
@@ -23,38 +27,6 @@ public class Student {
 
     @Column(name="age")
     private int age;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 
     @Override
     public String toString() {
